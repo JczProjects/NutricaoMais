e567e7c832588dbf3f82a8c981928217cebbe875
JczProjects <45704102+JczProjects@users.noreply.github.com>
2019-10-26 16:52:07 -0300

    Crição do projeto - Nutricao Mais
    Primeiro commit do projeto após a sua criação

---
+++ AlegriaDeViver/NutricaoMais/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NutricaoMais.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
